d3c89e6391707a248bbd27fe314f627bdb06b0c3
Mytsko Andrii <mytsko.andrii@gmail.com>
2020-01-05 14:51:36 +0200

    fix name space

--- BlogPost.Dal/Entities/BlogEntity.cs
+++ BlogPost.Dal/Entities/BlogEntity.cs
@@ -1,6 +1,5 @@
 ﻿using BlogPost.Dal.Identities;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlogPost.Dal.Entities
